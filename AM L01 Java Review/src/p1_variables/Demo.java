80d8fa90488af2111e954b7aeb052c56296cef34
B. Chen <chenb@sunysuffolk.edu>
2021-01-26 10:47:07 -0500

    Lecture 1

---
+++ AM L01 Java Review/src/p1_variables/Demo.java
@@ -0,0 +1,13 @@
+package p1_variables;
+
+public class Demo {
+
+	public static void main(String[] args) { // static method
+		hello();
+	}
+	
+	public static void hello() { // instance method
+		System.out.println("Hello!");
+	}
+
+}
